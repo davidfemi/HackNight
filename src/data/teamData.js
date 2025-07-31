54a36d16be185f4e6141c3a92d200a2331db0cd0
sidneymgomez <sidney.mgomezrodriguez@intercom.io>
2025-07-30 20:01:17 -0500

    feat: Add [Sidney M. Gomez]'s teammate profile

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -19,5 +19,15 @@ export const teamMembers = [
     skills: ["Looking at Kibana"],
     github: "https://github.com/bmbranca",
     email: "brian@hacknight.dev"
-  }
+  },
+  {
+  id: 967,
+  name: "Sidney M. Gomez",
+  role: "Island Girl",
+  image: "/images/IMG_6770.jpg",
+  bio: "HEY!! Hey! hey",
+  skills: ["being confused"],
+  github: "https://github.com/sidneymgomez",
+  email: "sidney@hacknight.dev"
+}
 ];
